ac88169d8f0ad6011bc2e83b12f9b5f50a9cc501
Enngage <ennspassion@gmail.com>
2018-02-26 14:49:54 +0100

    Disable tslint rule

--- src/ngx-captcha/utilities/guid-helper.class.ts
+++ src/ngx-captcha/utilities/guid-helper.class.ts
@@ -1,6 +1,7 @@
 export class GuidHelper {
      newGuid() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            // tslint:disable-next-line:triple-equals no-bitwise
             const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         });
