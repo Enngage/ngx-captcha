e1b406e94c15292bf8c5b7ab88dd66bf212c987f
Enngage <ennspassion@gmail.com>
2018-02-27 21:23:50 +0100

    Removes obsolete files

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./recaptcha/recaptcha-2.component"));
-__export(require("./recaptcha/invisible-recaptcha.component"));
-__export(require("./ngx-captcha.module"));
