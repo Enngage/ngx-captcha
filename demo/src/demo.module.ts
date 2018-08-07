a064a676573d4680146045729dd57e8625b4ea4f
Daniel Reiser <daniel.reiser@mhp.com>
2018-08-07 16:23:59 +0200

    Add FormsModule and ReactiveFormsModule to DemoModule

--- demo/src/demo.module.ts
+++ demo/src/demo.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
-import { NgxCaptchaModule } from '../../projects/ngx-captcha-lib/src/public_api';
+import { NgxCaptchaModule } from '../../projects/ngx-captcha-lib/src/lib';
 import { DemoComponent } from './demo.component';
 import { DemoRoutes } from './demo.routes';
 import { InstallationComponent } from './installation.component';
 import { InvisibleReCaptchaDemoComponent } from './invisible-recaptcha-demo.component';
 import { ReCaptcha2DemoComponent } from './re-captcha-2-demo.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     RouterModule,
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     NgxCaptchaModule.forRoot({
       reCaptcha2SiteKey: '6LcvoUgUAAAAAJJbhcXvLn3KgG-pyULLusaU4mL1',
       invisibleCaptchaSiteKey: '6LckpEgUAAAAACPcjmrg1Es-GnTltKx0MP61FBO8'
